3cc22dd079564bce6adb0c528b8d47ae53dbee3e
Hudson Gomes Torres <55596080+Nosduhh@users.noreply.github.com>
2023-07-12 17:39:06 -0300

    aprendendo javascript

---
+++ ex004/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 45
+console.log(`A velocidade está em ${velocidade}km/h`)
+if (velocidade >= 60) { //condição simples.
+    console.log(`Você ultrapassou a velocidade permitidade. MULTADO!!`)
+}
+else{ //condição composta.
+    console.log(`Dirija sempre com atenção!`)
+}
---
+++ ex004/ex009.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+console.log(`Você está vivendo no país ${país}`)
+if(país== 'Brasil'){
+    console.log('brasileiro')
+}
+else{
+    console.log('Estrangeiro')
+}
