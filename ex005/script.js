734da99cc0205df7e2089f68c15570a43755ccf2
Hudson Gomes Torres <55596080+Nosduhh@users.noreply.github.com>
2023-07-15 10:47:07 -0300

    conclusão do desafio do dia a dia

--- ex005/script.js
+++ ex005/script.js
@@ -2,17 +2,20 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
-    //var hora = 21
+    var hora = 15//data.getHours()
+
     msg.innerHTML = `O horário em tempo real é <strong>${hora}</strong> horas.`
     if (hora >= 0 && hora < 12) {
         //Bom dia!
-        img.src = 'amanhecermaior.png'
-    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/amanhecermaior.png'
+        document.body.style.background = '#E3BA49'
+    } else if (hora > 12 && hora < 18) {
         //Boa tarde!
-        img.src = 'entardecer.png'
+        img.src = 'imagens/entardecer.png'
+        document.body.style.background = '#FF921C'
     } else {
         //Boa noite!
-        img.src = 'anoitecer.png'
+        img.src = 'imagens/anoitecer.png'
+        document.body.style.background = '#011D43'
     }
 }
