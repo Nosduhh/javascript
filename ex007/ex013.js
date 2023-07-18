b34ba232a7495abcfaea1305163ea3c3f3f96fef
Hudson Gomes Torres <55596080+Nosduhh@users.noreply.github.com>
2023-07-18 12:22:54 -0300

    aprendendo estrutura de repetição for

---
+++ ex007/ex013.js
@@ -0,0 +1,7 @@
+// Estrutura de repetição usando for
+
+console.log('Seja bem vindo, a nossa pizzaria!')
+for (var pizza = 1; pizza <= 10; pizza++) {
+    console.log('Comeu a pizza!')
+}
+console.log('Parabens você comeu tudo!')
